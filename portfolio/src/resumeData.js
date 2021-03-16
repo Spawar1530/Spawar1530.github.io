f0d3932e7cd072031a21373743b418ee07041ef5
Suraj Pawar <spawar1530@gmail.com>
2021-03-16 14:53:19 +0530

    my portfolio

---
+++ portfolio/src/resumeData.js
@@ -0,0 +1,76 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/Spawar1530/my-portfolio.io",
+    "name": "Suraj Pawar",
+    "role": "Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/suraj-pawar-0888b510b//",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Spawar1530",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I have finished my B.E. in Chemical engineering and have been worked in industry for past few years, I have done my PGD from School of Accelerated Learning.",
+    "address":"Satara, Maharashtra, India - 415503",
+    "contact":"+91-7387714610",
+    "education":[
+      {
+        "UniversityName":"Savitribai Phule Pune University",
+        "specialization":"Bachelor of Enginnering in Chemical stream",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2019",
+        "Achievements":"Published research paper in elsevier. Qualified GATE 2019 with all india rank 2000."
+      },
+      {
+        "UniversityName":"School of Accelerated Learning",
+        "specialization":"Post Graduation Diploma in Web Development",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2021",
+      }
+    ],
+    "skillsDescription":"My Skills",
+    "skills":[
+      {
+        "skillname":"JAVASCRIPT"
+      },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"EV Smart",
+        "description":"Webapp to provide service to charge electric vehicle. Charging stations are provided by people for people.",
+        "imgurl":"images/portfolio/project2.png",
+        "url": "https://namangupta.xyz"
+      },
+      {
+        "name":"Slinger",
+        "description":"Chatting webapp - connect with your friends easily",
+        "imgurl":"images/portfolio/project.jpg",
+        "url": "https://slinger-chat.herokuapp.com/"
+      },
+      {
+        "name":"Black jack",
+        "description":"Webapp - Simple blackjack game developed in HTML and JS",
+        "imgurl":"images/portfolio/project2.png",
+        "url": "https://black-jack97.herokuapp.com/"
+      }
+    ],
+  }
+  
+  export default resumeData
+  
